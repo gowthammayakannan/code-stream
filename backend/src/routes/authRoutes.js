eda3cd72d66f1b437b205cf78a7cb609bef7d020
Gowtham <gowthammayakannan@gmail.com>
2026-02-02 17:26:26 +0530

    Security: Remove temporary DB cleanup routes after successful purge

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const { register, login, getMe, googleLogin, githubLogin, cleanupDB } = require('../controllers/authController');
+const { register, login, getMe, googleLogin, githubLogin } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/google', googleLogin);
 router.post('/github', githubLogin);
 router.get('/me', protect, getMe);
-router.get('/cleanup-db', cleanupDB);
 
 module.exports = router;
