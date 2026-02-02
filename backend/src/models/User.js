6e63e8a74812a5c402fd2982fa0f40ce3894b9e4
Gowtham <gowthammayakannan@gmail.com>
2026-02-02 16:58:03 +0530

    Fix: Add password hashing to User model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -56,6 +56,16 @@ const userSchema = new mongoose.Schema(
 
 
 
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
 // Compare password method
 userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
